2f534f26511961b7c090f1c439defdb0f9d50e30
HeeSungLee <a73482006@gmail.com>
2022-12-10 20:23:12 +0900

    list line

--- main.cpp
+++ main.cpp
@@ -36,26 +36,26 @@ class Order {
     queue<int> qMenu;
     void printIngredient() {
         cout << "          MENU         \n";
-        cout << " ----------------------\n";
-        cout << "|      햄      |   1   |\n";
-        cout << "|     치즈     |   2   |\n";
-        cout << "|     치킨     |   3   |\n";
-        cout << "|    양상추    |   4   |\n";
-        cout << "|    베이컨    |   5   |\n";
-        cout << "|    토마토    |   6   |\n";
-        cout << "|  소고기 패티 |   7   |\n";
-        cout << "|  불고기 패티 |   8   |\n";
-        cout << "|     김치     |   9   |\n";
-        cout << "|    초콜릿    |   10  |\n";
-        cout << "|     젤리     |   11  |\n";
-        cout << "|    누텔라    |   12  |\n";
-        cout << "|   마요네즈   |   13  |\n";
-        cout << "|     케첩     |   14  |\n";
-        cout << "|   스리라차   |   15  |\n";
-        cout << "|   청양고추   |   16  |\n";
-        cout << "|     된장     |   17  |\n";
-        cout << "|    삼겹살    |   18  |\n";
-        cout << " ----------------------\n";
+        cout << "┏━━━━━━━━━━━━━━┳━━━━━━━┓\n";
+        cout << "┃      햄      ┃   1   ┃\n";
+        cout << "┃     치즈     ┃   2   ┃\n";
+        cout << "┃     치킨     ┃   3   ┃\n";
+        cout << "┃    양상추    ┃   4   ┃\n";
+        cout << "┃    베이컨    ┃   5   ┃\n";
+        cout << "┃    토마토    ┃   6   ┃\n";
+        cout << "┃  소고기 패티 ┃   7   ┃\n";
+        cout << "┃  불고기 패티 ┃   8   ┃\n";
+        cout << "┃     김치     ┃   9   ┃\n";
+        cout << "┃    초콜릿    ┃   10  ┃\n";
+        cout << "┃     젤리     ┃   11  ┃\n";
+        cout << "┃    누텔라    ┃   12  ┃\n";
+        cout << "┃   마요네즈   ┃   13  ┃\n";
+        cout << "┃     케첩     ┃   14  ┃\n";
+        cout << "┃   스리라차   ┃   15  ┃\n";
+        cout << "┃   청양고추   ┃   16  ┃\n";
+        cout << "┃     된장     ┃   17  ┃\n";
+        cout << "┃    삼겹살    ┃   18  ┃\n";
+        cout << "┗━━━━━━━━━━━━━━┻━━━━━━━┛\n";
     }
     void plusIngredient() {
         int i, temp;
@@ -109,7 +109,7 @@ int main() {
     Stemp = s;
     cout << bread[0] << "\n";
     for (int i = 0; i < size; i++) {
-        cout << menu[a.Pop(&Stemp)] << "\n";
+        cout << menu[a.Pop(&Stemp) -1] << "\n";
     }
     cout << bread[1];
     return 0;
